9f0005c29519e5a0c726da2e6f59358853e5a568
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2022-02-11 18:36:36 -0800

    add react router and a mock login

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {BrowserRouter, NavLink, Route, Routes} from "react-router-dom"
+import CreateLogin from './CreateLogin.js'
+import Chat from './Chat.js'
 
 export default function Home() {
-  return <div>
+
+const [usernameInputValue, setUsernameInputValue] = useState('');
+const [passwordInputValue, setPasswordInputValue] = useState('');
+
+function handleClick() {
+    if (usernameInputValue !== "" && passwordInputValue !== "" ) {
+        console.log('liuakinwaekufhskjanusdhjkfabmjsahsfdjkhnsajvkfakfdj')
+    }
+}
+
+  return <BrowserRouter><div>
             <div className="container">
                 <h1
                     className="title"
@@ -9,24 +22,50 @@ export default function Home() {
                 <p
                     className="title_login"
                 >Please Login</p>
-                <div
-                    className="container_input"
-                >
-                    <input
-                        type="text"     
-                        placeholder="username"
-                        //value={inputValue}
-                        //onChange={event=>setInputValue(event.target.value)}> 
-                    ></input>
-                    <input
-                        type="password"     
-                        placeholder="password"
-                    ></input>
-                </div>
-                <button
-                    className="loginBtn"
-                >Let's Go</button>
+                <form>
+                    <div
+                        className="container_input"
+                    >
+                        
+                        <input
+                            type="text"     
+                            placeholder="username"
+                            value={usernameInputValue}
+                            onChange={event=>setUsernameInputValue(event.target.value)}
+                        ></input>
+                        <input
+                            type="password"     
+                            placeholder="password"
+                            value={passwordInputValue}
+                            onChange={event=>setPasswordInputValue(event.target.value)}
+                        ></input>
+                    </div>
+                        <button
+                            className="loginBtn"
+                            onClick={handleClick}
+                        >Let's Go
+                        </button>
+                        <p>New here? 
+                            <NavLink exact="true" to="/createlogin"
+                            className= "nav_link"
+                            >Create a login
+                            </NavLink > or 
+                            <Navlink exact="true" to="/chat"
+                            className= "nav_link"
+                            >view as a guest
+                            </Navlink>
+
+                        </p>
+                </form>
+                <Routes>
+                    <Route path="/createlogin" element={<CreateLogin />} />
+                    <Route path="/chat" element={<Chat />} />
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            
             </div>
 
-         </div>;
+         </div>
+         </BrowserRouter>
 }
+
