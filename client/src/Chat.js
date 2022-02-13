824bbd69d00a53c49bce46b410098477de54c729
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2022-02-12 17:02:27 -0800

    create a way to tab over based on language channels

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -4,6 +4,12 @@ import Usercard from './Usercard'
 
 export default function Chat() {
 
+const [englishtabclass, setEnglishtabclass] = useState('activeTab')
+const [spanishtabclass, setSpanishtabclass] = useState('inactiveTab')
+const [chinesetabclass, setChinesetabclass] = useState('inactiveTab')
+//const [message, setMessage] = useState('')
+//const [messageInputValue, setMessageInputValue] = useState('')
+
 const [users, setUsers] = useState(
     [
         {
@@ -27,14 +33,17 @@ const [users, setUsers] = useState(
 const [englishChats, setEnglishChats] = useState(
     [
        {
+            chat_id: 4,
             user_id: 3,
             text: "hey, what's upall."
         },
         {
+            chat_id: 5,
             user_id: 3,
             text: "I want tacos!"
         },
         {
+            chat_id: 6,
             user_id: 2,
             text: "??"
         }
@@ -45,14 +54,17 @@ const [englishChats, setEnglishChats] = useState(
 const [chineseChats, setChineseChats] = useState(
     [
         {
+            chat_id: 7,
             user_id: 3,
             text: "你好"
         },
         {
+            chat_id: 8,
             user_id: 3,
             text: "你从哪里来？"
         },
         {
+            chat_id: 9,
             user_id: 2,
             text: "我从 中国 来。"
         }
@@ -62,14 +74,17 @@ const [chineseChats, setChineseChats] = useState(
 const [spanishChats, setSpanishChats] = useState(
     [
         {
+            chat_id: 10,
             user_id: 3,
             text: "¿Cómo te llamas?"
         },
         {
+            chat_id: 11,
             user_id: 1,
             text: "Me llamo Kermit."
         },
         {
+            chat_id: 12,
             user_id: 2,
             text: "¿Me puede pasar la sal/la pimienta?"
         } 
@@ -81,8 +96,36 @@ const usersLoop = users.map(user => {
   })
 
 const englishChatsLoop = englishChats.map(chat=> {
-    return <p key={chat.user_id}>{chat.text}</p>
+    return <p key={chat.chat_id}>{chat.text}</p>
 })  
+
+const spanishChatsLoop = spanishChats.map(chat=> {
+    return <p key={chat.chat_id}>{chat.text}</p>
+})  
+
+const chineseChatsLoop = chineseChats.map(chat=> {
+    return <p key={chat.chat_id}>{chat.text}</p>
+})  
+
+function openTabEnglish() {
+    setEnglishtabclass('activeTab')
+    setSpanishtabclass('inactiveTab')
+    setChinesetabclass('inactiveTab')
+}
+function openTabSpanish() {
+    setEnglishtabclass('inactiveTab')
+    setSpanishtabclass('activeTab')
+    setChinesetabclass('inactiveTab')
+}
+function openTabChinese() {
+    setEnglishtabclass('inactiveTab')
+    setSpanishtabclass('inactiveTab')
+    setChinesetabclass('activeTab')
+}
+
+// function postMessage() {
+//     setMessage(messageInputValue)
+// }
 
   return <div>
       <div
@@ -97,14 +140,49 @@ const englishChatsLoop = englishChats.map(chat=> {
         <div
             className="chat_area"
         >
-            {englishChatsLoop}
+            <button
+                onClick={openTabEnglish}
+            >English</button>
+            <button
+                onClick={openTabSpanish}
+            >Espanol</button>
+            <button
+                onClick={openTabChinese}
+            >中文</button> 
+
+            <div
+                id="english"
+                className={englishtabclass}
+            >{englishChatsLoop}
+                {/* <p>{message}</p> */}
+            </div>
+            <div
+                id="spanish"
+                className={spanishtabclass}
+            >{spanishChatsLoop}
+                {/* <p>{message}</p> */}
+            </div>
+            <div
+                id="chinese"
+                className={chinesetabclass}
+            >{chineseChatsLoop}
+                {/* <p>{message}</p> */}
+            </div> 
         </div>
 
         <div
             className="message_area"
         >
-            <p>Type your message here! </p>
+            {/* <input
+                type='text'
+                placeholder='Type your message here!'
+                //onChange={event=>setMessageInputValue(event.target.value)}
+            > </input> */}
+            <button
+                //onClick={postMessage}
+            >post</button>
         </div>
+
       </div>
       <Link to="/"
             className= "nav_link">
