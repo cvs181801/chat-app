7cdeae4c1dbaf8ff54ea258dd82d0cc4d2cda87b
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2022-02-13 10:20:38 -0800

    move the chat messages over to server

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom"
 import Usercard from './Usercard'
 import axios from 'axios'
+import { unstable_renderSubtreeIntoContainer } from 'react-dom';
 
 export default function Chat() {
 
@@ -10,83 +11,23 @@ const [spanishtabclass, setSpanishtabclass] = useState('inactiveTab')
 const [chinesetabclass, setChinesetabclass] = useState('inactiveTab')
 //const [message, setMessage] = useState('')
 //const [messageInputValue, setMessageInputValue] = useState('')
+const [allmessages, setAllmessages] = useState('')
 
 const [users, setUsers] = useState([])
 
-const [englishChats, setEnglishChats] = useState(
-    [
-       {
-            chat_id: 4,
-            user_id: 3,
-            text: "hey, what's upall."
-        },
-        {
-            chat_id: 5,
-            user_id: 3,
-            text: "I want tacos!"
-        },
-        {
-            chat_id: 6,
-            user_id: 2,
-            text: "??"
-        }
+//**
+ 
+// const englishChats = englishChats.map(chat=> {
+//     return <p key={chat.chat_id}>{chat.text}</p>
+// })  
 
-    ]
-)
+// const spanishChats = spanishChats.map(chat=> {
+//     return <p key={chat.chat_id}>{chat.text}</p>
+// })  
 
-const [chineseChats, setChineseChats] = useState(
-    [
-        {
-            chat_id: 7,
-            user_id: 3,
-            text: "你好"
-        },
-        {
-            chat_id: 8,
-            user_id: 3,
-            text: "你从哪里来？"
-        },
-        {
-            chat_id: 9,
-            user_id: 2,
-            text: "我从 中国 来。"
-        }
-    ]
-)
-
-const [spanishChats, setSpanishChats] = useState(
-    [
-        {
-            chat_id: 10,
-            user_id: 3,
-            text: "¿Cómo te llamas?"
-        },
-        {
-            chat_id: 11,
-            user_id: 1,
-            text: "Me llamo Kermit."
-        },
-        {
-            chat_id: 12,
-            user_id: 2,
-            text: "¿Me puede pasar la sal/la pimienta?"
-        } 
-    ]
-)
-
-
-
-const englishChatsLoop = englishChats.map(chat=> {
-    return <p key={chat.chat_id}>{chat.text}</p>
-})  
-
-const spanishChatsLoop = spanishChats.map(chat=> {
-    return <p key={chat.chat_id}>{chat.text}</p>
-})  
-
-const chineseChatsLoop = chineseChats.map(chat=> {
-    return <p key={chat.chat_id}>{chat.text}</p>
-})  
+// const chineseChats = chineseChats.map(chat=> {
+//     return <p key={chat.chat_id}>{chat.text}</p>
+// })  
 
 function openTabEnglish() {
     setEnglishtabclass('activeTab')
@@ -118,17 +59,32 @@ async function getUsers() {
     }
 }
 
+async function getMessages() {
+    try {
+        var search = await axios.get('api/messages')
+        return search
+    }
+    catch(err) {
+        console.log(err)
+    }
+}
+
 useEffect(()=>{
     getUsers()
         .then((res) => {
-         console.log(res.data)
+         console.log('users :', res.data)
          setUsers([res.data])
     })
+    getMessages()
+        .then((res)=>{
+            console.log('messages :', res.data)
+            setAllmessages([res.data])
+        })
 },[])
 
 useEffect(()=> {
-    console.log(users)
-}, [users])
+    console.log('users :', users, 'allmessages :', allmessages)
+}, [users, allmessages])
 
 const usersLoop = users.map(user => {
     return <Usercard key={user.id} username={user.username}/>
@@ -160,20 +116,21 @@ const usersLoop = users.map(user => {
             <div
                 id="english"
                 className={englishtabclass}
-            >{englishChatsLoop}
-                {/* <p>{message}</p> */}
+             >
+            {/*{englishChats}
+                 <p>{message}</p> */}
             </div>
             <div
                 id="spanish"
                 className={spanishtabclass}
-            >{spanishChatsLoop}
-                {/* <p>{message}</p> */}
+            > {/* {spanishChats}
+                <p>{message}</p> */}
             </div>
             <div
                 id="chinese"
                 className={chinesetabclass}
-            >{chineseChatsLoop}
-                {/* <p>{message}</p> */}
+            > {/*{chineseChats}
+                 <p>{message}</p> */}
             </div> 
         </div>
 
--- server.js
+++ server.js
@@ -20,24 +20,6 @@ app.listen(port);
 //when moving the objects over here, ...the client needs to request the deata from an endpoint on server where data lives
 //
 
-//function getUser(id) {
-    //app.get(`api/user/${id}`, async (req, res) => {
-    //     const {search} = req.query;
-    //     console.log(search)
-    //     const response = await axios.get() 
-    //     res.send(response)
-    //     return response
-    // })
-    //res.send('a single user')
-//}
-
-//getUser()
-
-//app.get(`/api/user/${id}`, function (req, res) {
-//    res.send('one user!')
-//  })
-
-//function getUsers() {
 app.get(`/api/users`, function (req, res) {
     res.send(
                 [
@@ -58,4 +40,74 @@ app.get(`/api/users`, function (req, res) {
                     }
                 ]
             )
+})
+
+
+app.get(`/api/messages`, function(req, res) {
+    res.send(
+        [
+            {
+            English : 
+                    [
+                       {
+                            chat_id: 4,
+                            user_id: 3,
+                            text: "hey, what's upall."
+                        },
+                        {
+                            chat_id: 5,
+                            user_id: 3,
+                            text: "I want tacos!"
+                        },
+                        {
+                            chat_id: 6,
+                            user_id: 2,
+                            text: "??"
+                        }
+                
+                    ]
+            },
+            {
+            Spanish:  
+                    [
+                        {
+                            chat_id: 10,
+                            user_id: 3,
+                            text: "¿Cómo te llamas?"
+                        },
+                        {
+                            chat_id: 11,
+                            user_id: 1,
+                            text: "Me llamo Kermit."
+                        },
+                        {
+                            chat_id: 12,
+                            user_id: 2,
+                            text: "¿Me puede pasar la sal/la pimienta?"
+                        } 
+                    ]
+            },
+            {
+            Chinese:
+                    [
+                        {
+                            chat_id: 7,
+                            user_id: 3,
+                            text: "你好"
+                        },
+                        {
+                            chat_id: 8,
+                            user_id: 3,
+                            text: "你从哪里来？"
+                        },
+                        {
+                            chat_id: 9,
+                            user_id: 2,
+                            text: "我从 中国 来。"
+                        }
+                    ]
+            
+            }
+        ]
+    )
 })
